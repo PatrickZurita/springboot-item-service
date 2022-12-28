13dad43f89ad7e72cd2e88699f918a221dff0bfb
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2022-12-27 20:03:30 -0500

    Update ItemServiceImpl.java

--- src/main/java/com/trickapp/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/trickapp/springboot/app/item/models/service/ItemServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
