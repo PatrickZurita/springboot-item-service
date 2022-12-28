c5c84aecb04450de7f7b15059717e10d02661cb8
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2022-12-28 11:45:49 -0500

    update item service as eureka client
    desacoplando los urls remplazandolo con los nombres de los servicios
    eliminando Ribbon dependencias, ya que eureka lo tiene incorporado.

--- src/main/java/com/trickapp/springboot/app/item/SpringbootItemServiceApplication.java
+++ src/main/java/com/trickapp/springboot/app/item/SpringbootItemServiceApplication.java
@@ -2,11 +2,11 @@ package com.trickapp.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@RibbonClient(name = "products-service")
+@EnableEurekaClient 
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages = "com.trickapp.springboot.app.item.clients")
